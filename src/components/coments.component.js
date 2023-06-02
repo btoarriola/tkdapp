c4358d88bdee34d9dce9063dccccfffdb3123871
btoarriola <betokanon@gmail.com>
2023-06-02 14:34:57 -0600

    regreso

--- src/components/coments.component.js
+++ src/components/coments.component.js
@@ -1,35 +1,30 @@
 import React, { Component } from 'react';
 import KafkaService from "../services/kafka.service";
-import axios from 'axios';
 
 class CommentBox extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      comments: [],
-      commentText: ""
+      comments: []
     };
   }
-
-  componentDidMount() {
-    this.fetchComments();
-  }
-
-  fetchComments = async () => {
-    const id = this.props.id;
-    console.log("try ",id);
-    const uri = "https://mongoapi-service-btoarriola.cloud.okteto.net/api/comments";
-    
-    try {
-      const response = await axios.get(`${uri}/${id}`);
-      const comentarios = response.data ? response.data : [];
   
-      this.setState({ comments: comentarios });
-    } catch (error) {
-      console.log('Error al obtener los comentarios:', error);
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const comment = this.refs.comment.value.trim();
+    if (!comment) {
+      return;
     }
-  };
-
+    this.setState((prevState) => {
+      return {
+        comments: prevState.comments.concat(comment)
+      };
+    });
+    this.refs.commentForm.reset();
+    // Llamar a la función saveComment para enviar el comentario
+    this.saveComment(1,comment);
+  }
+  
   saveComment = (status, comment) => {
     let data = {
       id: 0,
@@ -41,38 +36,24 @@ class CommentBox extends Component {
     KafkaService.comment(this.props.email, this.props.id, comment);
   }
 
-  handleCommentChange = (e) => {
-    this.setState({ commentText: e.target.value });
-  };
-
   render() {
-    const { comments, commentText } = this.state;
+    const { comments } = this.state;
 
     return (
       <div>
-        <form ref="commentForm" onSubmit={this.saveComment}>
+        <form ref="commentForm" onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="comment">Agregar comentario:</label>
-            <textarea
-              className="form-control"
-              id="comment"
-              rows="3"
-              ref="comment"
-              value={commentText}
-              onChange={this.handleCommentChange}
-            ></textarea>
+            <textarea className="form-control" id="comment" rows="3" ref="comment"></textarea>
           </div>
           <button type="submit" className="btn btn-primary">Enviar</button>
         </form>
         {comments.length > 0 ?
-          <div className="comments-list">
-            {comments.map((comentario) => (
-              <div className="comment" key={comentario._id}>
-                <h5>{comentario.userId}</h5>
-                <p>{comentario.comment}</p>
-              </div>
+          <ul className="list-group list-group-flush">
+            {comments.map((comment, index) => (
+              <li key={index} className="list-group-item">{comment}</li>
             ))}
-          </div>
+          </ul>
           :
           <p>Aun no hay comentarios</p>
         }
